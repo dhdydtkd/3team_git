062bf0c8ea357f1ca09d1adb51c60a3a5daa97cb
gihong <tyu1375@gmail.com>
2023-11-30 17:42:01 +0900

    추가

--- test.py
+++ test.py
@@ -2,21 +2,19 @@ import streamlit as st
 import os
 import re
 
- # 여기에 streamit text.input에 값 넣기
-dir_path = "dummy"
 
-#전체파일 리스트
-all_Files = os.listdir(dir_path)
-print(f"=====전체 파일 {all_Files}=====")
-
-# 보안 검사에 통과하지 못한 파일 넣는 리스트
-# 바로 마스킹 하면 필요 없을듯.
-detection_Pass_File = set()
-detection_Nonpass_File = set()
 
 # 확장자 확인 필요..
 # 확장자를 확인해서 txt 바로 확인 가능한지, 변환 후 진행해야되는 것들 확인
-def text_checkL():
+def text_check(dir_path):
+    # 보안 검사에 통과하지 못한 파일 넣는 리스트
+    # 바로 마스킹 하면 필요 없을듯.
+    detection_Pass_File = set()
+    detection_Nonpass_File = set()
+
+    #전체파일 리스트
+    all_Files = os.listdir(dir_path)
+    print(f"=====전체 파일 {all_Files}=====")
     # 전체 파일 검사
     for file_Name in all_Files:
         file_Path = os.path.join(dir_path,file_Name)
@@ -39,6 +37,9 @@ def text_checkL():
                             
     print(f"검출 파일{detection_Nonpass_File} 미검출{detection_Pass_File}")
 
-
+if __name__ == '__main__':
+     # 여기에 streamit text.input에 값 넣기
+    dir_path = "dummy"
+    text_check(dir_path)
 
         
